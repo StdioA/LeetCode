06ed4a4c2eb74c2ec9aee7bfe7660407c38fc8e7
David Dai <stdioa@163.com>
2019-07-01 22:08:31 +0800

    2019-07-01 with 3 solutions

---
+++ powx-n.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+    negative := false
+    if n < 0 {
+        negative = true
+        n = -n
+	}
+    res := myPow(x, n / 2)
+    res *= res
+    if n % 2 == 1 {
+        res *= x
+    }
+    if negative {
+        res = 1 / res
+    }
+    return res
+}
+
+func main() {
+	fmt.Println(myPow(2.0000, -2))
+}
---
+++ word-break.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func wordBreak(s string, wordDict []string) bool {
+    result := make([]bool, len(s) + 1)
+    result[0] = true
+    
+    for i := 1; i <= len(s); i++ {
+        for _, word := range wordDict {
+			lw := len(word)
+            if ((i >= lw) && result[i - lw] && (s[i - lw: i] == word)) {
+                result[i] = true
+                break
+            }
+        }
+	}
+    return result[len(s)]
+}
+
+func main() {
+	s := "applepenapple"
+	wordDict := []string{"apple", "pen"}
+	fmt.Println(wordBreak(s, wordDict))
+}
