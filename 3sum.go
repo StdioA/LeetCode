e47faaaccecdc562250238dd880c479794820754
David Dai <stdioa@163.com>
2020-03-19 14:15:33 +0800

    2020-03-19 Rewrite 3sum
    https://leetcode.com/problems/3sum/discuss/542431/C%2B%2B-100ms14.4MB-clean-solution-with-comments

--- 3sum.go
+++ 3sum.go
@@ -2,47 +2,54 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func threeSum(nums []int) [][]int {
-	result := make([][]int, 0)
-	count := make(map[int]int)
-	for _, num := range nums {
-		count[num]++
+	var (
+		result [][]int
+		seen   = make(map[int]struct{})
+	)
+	if len(nums) == 0 {
+		return result
 	}
 
-	checkValid := func(a, b, c int, count map[int]int) bool {
-		for _, num := range([]int{c, a, b}) {
-			count[num]--
-			if count[num] < 0 {
-				return false
+	sort.Ints(nums)
+	for i, num := range nums {
+		// Skip duplicate search for the first num
+		if _, got := seen[num]; got {
+			continue
+		}
+
+		sum := -num
+		// search with double pointer
+		begin, end := i+1, len(nums)-1
+		for begin < end {
+			b, e := nums[begin], nums[end]
+			switch {
+			case b+e == sum:
+				result = append(result, []int{num, b, e})
+				seen[num] = struct{}{}
+
+				// Skip the duplicate
+				for nums[begin] == b && begin < end {
+					begin++
+				}
+				for nums[end] == e && begin < end {
+					end--
+				}
+			case b+e < sum:
+				begin++
+			default:
+				end--
 			}
 		}
-		return true
+		seen[num] = struct{}{}
 	}
-	for a, _ := range count {
-		for b, _ := range count {
-			c := -a-b
-			if !(a <= b && b <= c) {
-				continue
-			}
-			// copy map
-			tmpCount := map[int]int{
-				a: count[a],
-				b: count[b],
-				c: count[c],
-			}
-			if checkValid(a, b, c, tmpCount) {
-				result = append(result, []int{a, b, c})
-			}
-		}
-	}
-	
 	return result
 }
 
 func main() {
-	// l := []int{-1, 0, 1, 2, -1, -4}
-	l := []int{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}
-	fmt.Println(threeSum(l))
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Println(threeSum([]int{1, -1, -1, 0}))
 }
